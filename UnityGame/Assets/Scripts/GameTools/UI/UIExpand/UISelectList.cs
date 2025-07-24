4fab84e68f4bc1b17ea699da04cc87c064712889
TongZiGang <754383023@qq.com>
2025-07-24 15:28:26 +0800

    简单选择列表

---
+++ UnityGame/Assets/Scripts/GameTools/UI/UIExpand/UISelectList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace CreatGame.UI
+{
+    public delegate void OnSelectCallback(int index);
+    /// <summary>
+    /// 选中列表
+    /// </summary>
+    public class UISelectList : MonoBehaviour
+    {
+        public OnSelectCallback onSelectCallback;
+        private List<Button> m_buttonList = new List<Button>();
+        private void Awake()
+        {
+            m_buttonList = new List<Button>();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Start()
+        {
+            var childCount= transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                var button = transform.GetChild(i).GetComponent<Button>();
+                if (button != null)
+                {
+                    m_buttonList.Add(button);
+                    button.onClick.AddListener(() => { OnClick(button); });
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="btn"></param>
+        private void OnClick(Button btn)
+        {
+            onSelectCallback?.Invoke(m_buttonList.IndexOf(btn));
+        }
+    }
+}
