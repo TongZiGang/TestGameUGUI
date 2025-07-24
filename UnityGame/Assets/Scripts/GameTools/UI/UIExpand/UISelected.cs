6f8d748a600b6bef7a07528100680e766d64ae25
TongZiGang <754383023@qq.com>
2025-07-24 15:15:59 +0800

    UI拓展

---
+++ UnityGame/Assets/Scripts/GameTools/UI/UIExpand/UISelected.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CreatGame.UI
+{
+    public class UISelected : MonoBehaviour
+    {
+        public int index = -1;
+        /// <summary>
+        /// 选中状态
+        /// </summary>
+        public GameObject selected;
+        /// <summary>
+        /// 未选中状态
+        /// </summary>
+        public GameObject unSelected;
+    }
+}
